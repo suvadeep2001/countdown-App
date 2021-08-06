9793b1ba36c8fe0736eb556b2854d7789023857e
Suvadeep <suvadeepmaity18@gmail.com>
2021-08-06 16:42:08 +0530

    added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <h1>Counter App Using State</h1>
       </header>
 
-      <h2>Current Value of Count is {count}</h2>
+      <h2>Current Value of Count is : {count}</h2>
       <button onClick={() => setCount(0)}>Reset Counter</button>
       <button onClick={() => count >= 10 ? "" : setCount(count + 1)}>Increase Counter</button>
       <button onClick={() => count > 0 ? setCount(count - 1) : ""}>Decrease Counter</button>
